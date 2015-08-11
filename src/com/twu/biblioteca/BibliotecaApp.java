b1fbdf20668dbcbc52b0ad3b65b2e4e02f4e35bb
xushanchuan <scxu@thoughtworks.com>
2015-08-11 11:41:01 +0800

    handle user input in a loop

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -3,12 +3,35 @@ package com.twu.biblioteca;
 public class BibliotecaApp {
 
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        BibliotecaApp app = new BibliotecaApp();
+        app.run();
     }
 
     public void run() {
+        boolean alive = true;
+        int cmd;
+        UserInputHandler userInputHandler = new UserInputHandler();
+
         welcome();
         showMainMenu();
+
+        while (alive == true) {
+            cmd = userInputHandler.getInput("Select One Option:");
+            if (cmd == 0) {
+                alive = false;
+            } else {
+                handleCmd(cmd);
+            }
+        }
+    }
+
+    private void handleCmd(int cmd) {
+        switch (cmd) {
+            case 1:
+                System.out.println("Show books"); break;
+            default:
+                System.out.println("Select a valid option!");
+        }
     }
 
     private void welcome() {
@@ -19,7 +42,7 @@ public class BibliotecaApp {
     private void showMainMenu() {
         String[] options = {"List Books", "Quit"};
 
-        System.out.println("Select action you want:");
+        System.out.println("Select action you want");
         for (String option : options) {
             System.out.println(option);
         }
