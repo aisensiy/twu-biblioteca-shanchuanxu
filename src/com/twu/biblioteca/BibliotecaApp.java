00b545c85fd1b65a3b82140311bda57fc13453f0
xushanchuan <scxu@thoughtworks.com>
2015-08-11 11:00:01 +0800

    show welcome message at start

--- src/com/twu/biblioteca/BibliotecaApp.java
+++ src/com/twu/biblioteca/BibliotecaApp.java
@@ -5,4 +5,13 @@ public class BibliotecaApp {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
     }
+
+    public void run() {
+        welcome();
+    }
+
+    private void welcome() {
+        String msg = "Welcome";
+        System.out.println(msg);
+    }
 }
---
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -0,0 +1,21 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.Assert.*;
+
+public class BibliotecaAppTest {
+    @Test
+    public void getWelcomeWhenStart() {
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outputStream));
+
+        BibliotecaApp app = new BibliotecaApp();
+        app.run();
+        assertTrue(outputStream.toString().startsWith("Welcome"));
+    }
+}
